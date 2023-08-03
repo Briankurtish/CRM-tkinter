8f955d7c31404cb76f3a0903e772c2ceb7810995
cipher <asaforbrian18@gmail.com>
2023-08-03 18:06:42 +0300

    added comments for the structure

--- treebase.py
+++ treebase.py
@@ -5,4 +5,18 @@ root = Tk()
 root.title("Cipher - TreeBase")
 root.geometry("1000x500")
 
+#Add some style
+
+#Pick a theme
+
+#Configure the treeview colors
+
+#Change selected color
+
+#Create a treeview frame
+
+#Create a treeview Scrollbar
+
+#Create the treeview
+
 root.mainloop()
