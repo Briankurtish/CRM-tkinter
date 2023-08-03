11c36f9ea401a1ad388282ef07ab2b3355de6c25
cipher <asaforbrian18@gmail.com>
2023-08-03 18:13:55 +0300

    Added some style and a theme

--- treebase.py
+++ treebase.py
@@ -6,9 +6,10 @@ root.title("Cipher - TreeBase")
 root.geometry("1000x500")
 
 #Add some style
+style = ttk.Style()
 
 #Pick a theme
-
+style.theme_use('default')
 #Configure the treeview colors
 
 #Change selected color
@@ -18,5 +19,22 @@ root.geometry("1000x500")
 #Create a treeview Scrollbar
 
 #Create the treeview
+
+#Configure the scrollbar
+
+#define our columns
+
+#Format our columns
+
+#Create Headings
+
+#Add Fake data
+
+#Create stripped row tags
+
+#Add Record entry boxes
+
+
+# Add Buttons
 
 root.mainloop()
